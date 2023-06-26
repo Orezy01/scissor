dd0027ff3826ee2dc9b178d32c51414b7d65192d
Oreoluwa <orezyjosh@gmail.com>
2023-06-26 23:16:20 +0100

    new change

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,11 +2,37 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div className="flex justify-center items-center">
-      <div>
-        
-      </div>
-    </div>
+    <section className="container forms">
+            <div className="form login">
+                <div className="form-content">
+                    <header>Login</header>
+
+                    <form action="#">
+                        <div className="field input-field">
+                            <input type="email" className="input" placeholder="Email" />
+                        </div>
+
+                        <div className="field input-field">
+                            <input type="password" className="input" placeholder="Password" />
+                        </div>
+
+                        <div className='form-link'>
+                          <a href="#" className='forgot-pass'>Forgot Password?</a>
+                        </div>
+
+                        <div className="field button-field">
+                            <button>Login</button>
+                        </div>
+
+                        <div className='form-link'>
+                          <span>Already have an account?
+                            <a href="#" className='signup-link'>Signup</a>
+                          </span>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </section>
   )
 }
 
---
+++ src/components/SignUp.tsx
@@ -0,0 +1,23 @@
+const SignUp = () => {
+    return(
+        <section className="container forms">
+            <div className="form login">
+                <div className="form-content">
+                    <header>Login</header>
+
+                    <form action="#">
+                        <div className="field input-field">
+                            <input type="email" className="input" placeholder="Email" />
+                        </div>
+
+                        <div className="field input-field">
+                            <input type="email" className="input" placeholder="Email" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default SignUp;
